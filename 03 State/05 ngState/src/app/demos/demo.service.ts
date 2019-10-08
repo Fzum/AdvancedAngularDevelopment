14530a0eba40461a0a3e63b53ff40006b17ec486
Alexander Pajer <alexander.pajer@integrations.at>
2019-10-08 12:37:23 +0200

    service to stateful

--- 03 State/05 ngState/src/app/demos/demo.service.ts
+++ 03 State/05 ngState/src/app/demos/demo.service.ts
@@ -1,18 +1,26 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, BehaviorSubject } from "rxjs";
-import { DemoItem } from "./demo-item.model";
-import { tap } from "rxjs/operators";
-import { environment } from "src/environments/environment";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { DemoItem } from './demo-item.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class DemoService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+    this.httpClient
+      .get<DemoItem[]>(`${environment.apiUrl}demos`)
+      .subscribe(data => (this.arrDemos = data));
+  }
+
+  private arrDemos: DemoItem[] = [];
+  private demos: BehaviorSubject<DemoItem[]> = new BehaviorSubject(
+    this.arrDemos
+  );
 
   getDemos(): Observable<DemoItem[]> {
-    return this.httpClient.get<DemoItem[]>(`${environment.apiUrl}demos`);
+    return this.demos;
   }
 
   delete(item: DemoItem) {
