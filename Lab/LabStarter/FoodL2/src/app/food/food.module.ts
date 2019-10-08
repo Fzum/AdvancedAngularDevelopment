68c4691c8094089e053bfd8188df80b2f9362ce4
Alexander Pajer <alexander.pajer@integrations.at>
2019-10-08 19:56:53 +0200

    L2 Food Starter

---
+++ Lab/LabStarter/FoodL2/src/app/food/food.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FoodContainerComponent } from './food-container/food-container.component';
+import { FoodListComponent } from './food-list/food-list.component';
+import { FoodEditComponent } from './food-edit/food-edit.component';
+import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [FoodContainerComponent, FoodListComponent, FoodEditComponent],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule]
+})
+export class FoodModule {}
